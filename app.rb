3d344cdc5313648c783547348beff1aa372aaeeb
saboyutaka <yutaka.paradigm.shift@gmail.com>
2018-11-19 20:03:16 +0900

    DBを使って記事を保存する

--- app.rb
+++ app.rb
@@ -1,7 +1,19 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
+require 'mysql2'
+
 json_path = File.dirname(__FILE__) + '/data/data.json'
+
+def db
+  @db ||= Mysql2::Client.new(
+    host:     ENV['DB_HOST'] || 'localhost',
+    port:     ENV['DB_PORT'] || '3306',
+    username: ENV['DB_USERNAME'] || 'root',
+    password: ENV['DB_PASSWORD'] || '',
+    database: ENV['DB_DATABASE'] || 'tutorial',
+  )
+end
 
 set :public_folder, 'public'
 
@@ -74,6 +86,31 @@ post '/form' do
 
   redirect '/form'
 end
+
+get "/posts" do
+  sql = "SELECT * FROM posts"
+  @posts = db.query(sql).to_a
+
+  erb :posts
+end
+
+post "/posts" do
+  title = params[:title]
+  filename = params[:file][:filename]
+  file = params[:file][:tempfile]
+
+  File.open("./public/image/#{filename}", 'wb') do |f|
+    f.write(file.read)
+  end
+
+  sql = "INSERT INTO `posts` (`title`, `image`, `created_at`) VALUES ('#{title}', '#{filename}', NOW());"
+  db.query(sql)
+
+  @posts = db.query("SELECT * FROM posts").to_a
+
+  redirect '/posts'
+end
+
 
 get '/session' do
   @name = session[:name]
